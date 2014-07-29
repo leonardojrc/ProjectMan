04f63a14bf3a38ff2d83b8e1ce8603891fa23d90
Leonardo <Leonardo@Leo-pc>
2014-07-29 00:52:29 -0400

    Primer Commit

---
+++ ProjectMan/src/main/java/leosoft/projects/ProjectMan/App.java
@@ -0,0 +1,13 @@
+package leosoft.projects.ProjectMan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
